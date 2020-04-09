1073408a5c602ccf71095593d61d2cdcdbad7b6a
Michael Passade <michael.passade@codeimmersives.com>
2020-04-09 15:48:09 -0400

    done

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 class Vehicle {
+  constructor(name,wheelCount,maxSpeed){
+    this.name = name
+    this.wheelCount = wheelCount
+    this.maxSpeed = maxSpeed
+    this.speed = 0
+  }
+  accelerate(){
+    if (this.maxSpeed/3 + this.speed <= this.maxSpeed){
+      this.speed += this.maxSpeed/3
+    }
+  }
 }
 
-class Car {
+class Car extends Vehicle {
+  constructor(name,maxSpeed){
+    super(name, 4, maxSpeed)
+  }
+  drive(){
+    return 'Vroom. '
+  }
 }
 
-class Ferrari {
+class Ferrari extends Car{
+  constructor(){
+    super('Ferrari', 180)
+  }
+  drive(){
+    return "Vroom. I'm a Ferrari!"
+  }
+  accelerate(){
+    if (this.maxSpeed/2 + this.speed <= this.maxSpeed){
+      this.speed += this.maxSpeed/2
+    }
+  }
 }
 
 
